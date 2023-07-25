2377b93c06c50571c4e26a3c64b2033f743c57a5
Pepe <95196085+PePeWee07@users.noreply.github.com>
2023-07-25 08:06:45 -0500

    gurdar  personas

--- codigo_funcional/codificar2.py
+++ codigo_funcional/codificar2.py
@@ -15,6 +15,8 @@ import base64
 import numpy as np
 import pymongo
 
+#imagesPath = "./imagen/grupo.png"
+
 app = Flask(__name__)
 CORS(app, resources={
     r"/guardar": {"origins": "http://localhost:4200"},
@@ -114,16 +116,6 @@ def upload_file():
         # Lista para almacenar los objetos JSON
         json_list = []
 
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            resp = jsonify({'message' : 'No file part in the request'})
-            resp.status_code = 400
-            return resp
-        
-        if not os.path.exists("imagen"):
-            os.makedirs("imagen")
-            print("Nueva carpeta creada: imagen")
-            
         #capturamos archivo subido
         imagesPath = request.files['file']
         #obtenemos el nombre del archivo
@@ -155,84 +147,67 @@ def upload_file():
         image = cv2.imread(output_file)
 
         # Convertir imagen de BGR a RGB
-        #print(image.shape)
+        print(image.shape)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         plt.imshow(image)
         #plt.show()
 
         # Detectar caras en la imagen
         boxes, _ = mtcnn.detect(image)
-    
-
-        try:
-            # Aquí se realiza el corte de las imágenes
-            if boxes is not None:
-                i = 1
-                for box in boxes:
-                    box = [int(coord) for coord in box]
-                    face_image_encodings = face_recognition.face_encodings(image, known_face_locations=[box])[0]
-                    #print(face_image_encodings)
-                    face = image[box[1]:box[3], box[0]:box[2], :]
-                    cv2.imwrite("faces/" + str(i) + ".png", cv2.cvtColor(face, cv2.COLOR_BGR2RGB))
-                    #i += 1
-                    #cv2.imshow("face", cv2.cvtColor(face, cv2.COLOR_BGR2RGB))
-                    cv2.waitKey(0)
-                    
-                    
-                    #------------Codificar Code: Codificamos cada una de las imagenes------------
-                    output_file_faces = "./faces/" + str(i) + ".png"
-                    imagen_file = cv2.imread(output_file_faces)
-
-                    face_locations = face_recognition.face_locations(imagen_file, number_of_times_to_upsample=2, model="hog")
-                    print("FaceLocation: ", len(face_locations))
-                    
-                    imgBase64 = base64.b64encode(open('./faces/' + str(i) + ".png", 'rb').read()).decode('utf-8')
-                    i += 1
-                    if len(face_locations) >= 0:
-                        face_loc = face_locations[0]
-                    
-                        face_image_encodings = face_recognition.face_encodings(imagen_file, known_face_locations=[face_loc])[0]
-                        #print("face_image_encondings:", face_image_encodings)
 
 
-                        if not os.path.exists("archivosjson"):
-                            os.makedirs("archivos")
+        # Aquí se realiza el corte de las imágenes
+        if boxes is not None:
+            i = 1
+            for box in boxes:
+                box = [int(coord) for coord in box]
+                face_image_encodings = face_recognition.face_encodings(image, known_face_locations=[box])[0]
+                #print(face_image_encodings)
 
-                        # Obtener el nombre base del archivo sin la extensión
-                        base_name = os.path.splitext(os.path.basename(output_file_faces))[0]
-                        print("base_name:", base_name)
-                        
-                        # Crear diccionario con la información de la cara
-                        face_data = {
-                            "encoding": face_image_encodings.tolist(),
-                            'imagen': imgBase64,
-                        }
-                        #print("face_dict:", face_data)
+                face = image[box[1]:box[3], box[0]:box[2], :]
+                cv2.imwrite("faces/" + str(i) + ".png", cv2.cvtColor(face, cv2.COLOR_BGR2RGB))
+                #cv2.imshow("face", cv2.cvtColor(face, cv2.COLOR_BGR2RGB))
+                cv2.waitKey(0)
 
-                        cv2.rectangle(imagen_file, (face_loc[3], face_loc[0]),(face_loc[1], face_loc[2]), (0, 255, 0))
-                        
-                        # Guardar diccionario en archivo json en el directorio "archivos"
-                        with open(f"archivosjson/{base_name}.json", "w") as f:
-                            json.dump(face_data, f)
+                # ----------Codificar imagen----------
+                output_file_faces = "./faces/" + str(i) + ".png"
+
+                imgBase64 = base64.b64encode(open('./faces/' + str(i) + ".png", 'rb').read()).decode('utf-8')
+
+                if not os.path.exists("archivosjson"):
+                    os.makedirs("archivos")
+
+                # Obtener el nombre base del archivo sin la extensión
+                base_name = os.path.splitext(os.path.basename(output_file_faces))[0]
+                
+                # Crear diccionario con la información de la cara
+                face_data = {
+                    "encoding": face_image_encodings.tolist(),
+                    'imagen': imgBase64,
+                }
+
+                # Guardar diccionario en archivo json en el directorio "archivos"
+                with open(f"archivosjson/{base_name}.json", "w") as f:
+                    json.dump(face_data, f)
                             
-                        # Agregar el JSON a la lista
-                        json_list.append(face_data)
-                        
-                    else:
-                        resp = jsonify({'error': "No se ha detectado ninguna cara en la imagen"})
-                        resp.status_code = 201
-                        return resp
-        except Exception as e:
-            print("Error-Bucle:", str(e))
-        
+                # Agregar el JSON a la lista
+                json_list.append(face_data)
+
+                i += 1
+        else:
+            resp = jsonify({'error': "No se ha detectado ninguna cara en la imagen"})
+            resp.status_code = 201
+            return resp
+
         resp = jsonify({'server': json_list})
         resp.status_code = 201
         return resp
-    
+
     except Exception as e:
-            resp = jsonify({'error': 'Error interno del servidor', 'message': str(e)})
-            resp.status_code = 500
-            return resp
+        resp = jsonify({'error': 'Error interno del servidor', 'message': str(e)})
+        resp.status_code = 500
+        return resp
+
 
 if __name__ == '__main__':
     app.run()
